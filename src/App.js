95628e25a9902ae84815b3e6de457c2f2891ac93
mozes32 <moussaebrahem32@gmail.com>
2022-06-24 21:15:13 +0200

    feat: Header tabs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Box } from "@mui/system";
 import CssBaseline from "@mui/material/CssBaseline";
 import Header from "components/Header";
+import OptionsTab from "components/OptionsTab";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
         <Box>
           <Header />
+          <OptionsTab />
         </Box>
       </Box>
     </>
