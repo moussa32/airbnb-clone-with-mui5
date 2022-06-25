5b6c17b756ae2d603d6cc2743a2642b3e7e922fa
mozes32 <moussaebrahem32@gmail.com>
2022-06-25 14:03:53 +0200

    feat: Add Carousel Card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Box } from "@mui/system";
 import CssBaseline from "@mui/material/CssBaseline";
 import Header from "components/Header";
 import OptionsTab from "components/OptionsTab";
+import LocationCards from "components/LocationCards";
+import Container from "@mui/material/Container";
 import "./App.css";
 
 function App() {
@@ -12,6 +14,11 @@ function App() {
         <Box>
           <Header />
           <OptionsTab />
+        </Box>
+        <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1, hight: 100, overflowY: "scroll" }}>
+          <Container maxWidth="xl" sx={{ mb: 3 }}>
+            <LocationCards />
+          </Container>
         </Box>
       </Box>
     </>
---
+++ src/components/LocationCards.js
@@ -0,0 +1,26 @@
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import { locations as cardLocations } from "data/mock-data";
+import CarouselCard from "./CarouselCard";
+import { useState } from "react";
+
+const LocationCards = () => {
+  const [cards] = useState(cardLocations);
+  if (!cards.length) {
+    return null;
+  }
+
+  return (
+    <Box sx={{ mx: 2 }}>
+      <Grid container rowSpacing={3} columnSpacing={3}>
+        {cards.map((location) => (
+          <Grid key={location.id} item xs={12} sm={4} md={4} lg={3}>
+            <CarouselCard location={location} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default LocationCards;
